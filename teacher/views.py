8a4f27f83932dc76770eaeee2260b54bd64eb4ac
Samveg12 <samveg.shah@spit.ac.in>
2021-11-18 21:22:57 +0530

    basic setup

--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django .http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.core.mail import send_mail
+import datetime
+from django.utils import timezone
 # Create your views here.
+
+def index(request):
+    return render(request,'teacher/index.html')
